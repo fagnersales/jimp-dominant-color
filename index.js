24a8441d29a4816ae97e014ebc3733fbffaeeb04
Fagner Sales <fs.fagnersales@gmail.com>
2020-12-13 18:51:16 -0300

    Sistema finalizado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { dominantColor } = require('./lib')
 
-const URL_FOR_IMAGE = 'https://cdn.discordapp.com/avatars/474407357649256448/0f215fa0163c2a30e4bcc4505e4161cd.png?size=512'
+const URL_FOR_IMAGE = 'https://cdn.discordapp.com/avatars/617739561488875522/f404695b5d2a94bfdff7fea7f0a6868a.png?size=2048'
 
-dominantColor(URL_FOR_IMAGE)
+dominantColor(URL_FOR_IMAGE, 10, false)
 .then(console.log)
